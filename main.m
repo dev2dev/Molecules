a433d6009ea89747a70f812de182094a23137dd3
Brad Larson <larson@sunsetlakesoftware.com>
2008-10-02 20:28:43 -0500

    Version 1.2

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PortaMol
+//
+//  Created by Brad Larson on 5/18/2008.
+//  Copyright SonoPlot, Inc. 2008. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+	
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+	int retVal = UIApplicationMain(argc, argv, nil, nil);
+	[pool release];
+	return retVal;
+}
